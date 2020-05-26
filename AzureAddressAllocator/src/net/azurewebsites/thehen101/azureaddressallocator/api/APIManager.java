f856a78dbf63c4f6cc0caba6f051e0c07748c8ea
thehen101 <thehen101@live.com>
2020-05-26 19:37:01 +0100

    Implemented API registration/unregistration

--- AzureAddressAllocator/src/net/azurewebsites/thehen101/azureaddressallocator/api/APIManager.java
+++ AzureAddressAllocator/src/net/azurewebsites/thehen101/azureaddressallocator/api/APIManager.java
@@ -1,12 +1,15 @@
 package net.azurewebsites.thehen101.azureaddressallocator.api;
 
+import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import net.azurewebsites.thehen101.azureaddressallocator.Allocator;
 import net.azurewebsites.thehen101.azureaddressallocator.file.FileRegistry;
@@ -18,6 +21,7 @@ import net.azurewebsites.thehen101.azureaddressallocator.file.FileRegistry;
  */
 public class APIManager {
 	private static final Logger LOGGER = LoggerFactory.getLogger(APIManager.class);
+	private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
 	
 	private final HashMap<String, String> userToKey = new HashMap<String, String>();
 	private final HashMap<String, String> keyToUser = new HashMap<String, String>();
@@ -33,15 +37,37 @@ public class APIManager {
 		this.loadEntries();
 	}
 	
+	public void register(final String user) {
+		final String key = this.generateKey();
+		this.userToKey.put(user, key);
+		this.keyToUser.put(key, user);
+		this.saveEntries();
+	}
+	
+	public void unregister(final String user) {
+		final String key = this.userToKey.get(user);
+		this.userToKey.remove(user);
+		this.keyToUser.remove(key);
+		this.saveEntries();
+	}
+	
+	public String userToKey(final String user) {
+		return this.userToKey.get(user);
+	}
+	
+	public String keyToUser(final String key) {
+		return this.keyToUser.get(key);
+	}
+	
 	private void loadEntries() {
 		this.userToKey.clear();
 		this.keyToUser.clear();
 		final String users = this.usersFile.getFileAsString(this.allocator);
 		final Gson gson = new Gson();
-		if (users.length() == 0)
+		if (users.length() == 0) {
 			//new file
-			this.usersFile.writeFile(this.allocator, gson.toJson(new String[0][2]));
-		else {
+			this.saveEntries();
+		} else {
 			final String[][] entries = gson.fromJson(users, String[][].class);
 			
 			for (int i = 0; i < entries.length; i++) {
@@ -50,6 +76,37 @@ public class APIManager {
 				this.keyToUser.put(entry[1], entry[0]);
 			}
 		}
-		LOGGER.debug("API entries loaded");
+		LOGGER.debug(this.userToKey.size() + " API entries loaded");
+	}
+	
+	private void saveEntries() {
+		final String[][] users = new String[this.userToKey.size()][2];
+
+		int index = 0;
+		for (final Entry<String, String> e : this.userToKey.entrySet()) {
+			users[index][0] = e.getKey();
+			users[index][1] = e.getValue();
+			index++;
+		}
+
+		this.usersFile.writeFile(this.allocator, new GsonBuilder().setPrettyPrinting().create().toJson(users));
+		LOGGER.debug(this.userToKey.size() + " API entries saved");
+	}
+	
+	private String generateKey() {
+		final SecureRandom sr = new SecureRandom();
+		final byte[] key = new byte[32];
+		sr.nextBytes(key);
+		return bytesToHex(key);
+	}
+
+	public static String bytesToHex(final byte[] bytes) {
+		final char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			final int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
 }
--- AzureAddressAllocator/src/net/azurewebsites/thehen101/azureaddressallocator/api/HTTPHandler.java
+++ AzureAddressAllocator/src/net/azurewebsites/thehen101/azureaddressallocator/api/HTTPHandler.java
@@ -1,11 +1,18 @@
 package net.azurewebsites.thehen101.azureaddressallocator.api;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.annotations.SerializedName;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
@@ -25,13 +32,124 @@ public class HTTPHandler {
 	public HTTPHandler(final Allocator allocator, final int bindPort) throws IOException {
 		this.allocator = allocator;
 		this.httpServer = HttpServer.create(new InetSocketAddress(bindPort), 0);
-		LOGGER.debug("HttpServer created");
+		LOGGER.debug("HttpServer created on port " + bindPort + ": " + this.httpServer.toString());
 		
-		this.httpServer.createContext("/register", (HttpExchange exchange) -> {
-			//this.allocator.
-			//exchange.
+		this.httpServer.createContext("/register", (final HttpExchange exchange) -> {
+			//only allow API registrations from localhost!
+			if (!exchange.getRemoteAddress().getAddress().isLoopbackAddress()) {
+				this.sendResponse(exchange, 403 /* FORBIDDEN */, 
+						new APIResponse("error", "Only localhost can register users!"));
+			} else {
+				final String post = this.getRequestData(exchange);
+				try {
+					final Gson gson = new Gson();
+					final String[] users = gson.fromJson(post, String[].class);
+					
+					//make sure we don't already have a registration
+					for (int i = 0; i < users.length; i++) {
+						if (this.allocator.getAPIManager().userToKey(users[i]) != null) {
+							this.sendResponse(exchange, 400 /* BAD REQUEST */, new APIResponse("error",
+									"One or more users are already registered. No users have been registered."));
+							return;
+						}
+					}
+					
+					for (int i = 0; i < users.length; i++) {
+						this.allocator.getAPIManager().register(users[i]);
+						LOGGER.info("Registered API user: " + users[i] + " with API key "
+								+ this.allocator.getAPIManager().userToKey(users[i]));
+					}
+					this.sendResponse(exchange, 200 /* OK */,
+							new APIResponse("success", users.length + " users registered."));
+				} catch (final JsonSyntaxException jse) {
+					this.sendResponse(exchange, 400 /* BAD REQUEST */, 
+							new APIResponse("error", "Malformed JSON. Requires a String array."));
+				}
+			}
+		});
+
+		this.httpServer.createContext("/unregister", (final HttpExchange exchange) -> {
+			// only allow API UNregistrations from localhost!
+			if (!exchange.getRemoteAddress().getAddress().isLoopbackAddress()) {
+				this.sendResponse(exchange, 403 /* FORBIDDEN */,
+						new APIResponse("error", "Only localhost can unregister users!"));
+			} else {
+				final String post = this.getRequestData(exchange);
+				try {
+					final Gson gson = new Gson();
+					final String[] users = gson.fromJson(post, String[].class);
+
+					for (int i = 0; i < users.length; i++) {
+						if (this.allocator.getAPIManager().userToKey(users[i]) == null) {
+							this.sendResponse(exchange, 400 /* BAD REQUEST */, new APIResponse("error",
+									"One or more users are not registered. No users have been unregistered."));
+							return;
+						}
+					}
+
+					for (int i = 0; i < users.length; i++) {
+						this.allocator.getAPIManager().unregister(users[i]);
+						LOGGER.info("Unregistered API user: " + users[i]);
+					}
+					this.sendResponse(exchange, 200 /* OK */,
+							new APIResponse("success", users.length + " users unregistered."));
+				} catch (final JsonSyntaxException jse) {
+					this.sendResponse(exchange, 400 /* BAD REQUEST */,
+							new APIResponse("error", "Malformed JSON. Requires a String array."));
+				}
+			}
 		});
 		
+		this.httpServer.setExecutor(this.allocator.getRequestThreads());
+		this.httpServer.start();
 		LOGGER.info("HTTPHandler initialised");
+	}
+	
+	private String getRequestData(final HttpExchange exchange) {
+		//if your IDE shows the following line as a warning, it's wrong, useDelimiter returns this
+		final Scanner scanner = new Scanner(exchange.getRequestBody()).useDelimiter("\\A");
+		try {
+			if (!scanner.hasNext()) {
+				scanner.close();
+				return null;
+			} else {
+				final String post = URLDecoder.decode(scanner.next(), StandardCharsets.UTF_8.name());
+				scanner.close();
+				return post;
+			}
+		} catch (final UnsupportedEncodingException e) {
+			e.printStackTrace();
+			scanner.close();
+			return null;
+		}
+	}
+	
+	private void sendResponse(final HttpExchange exchange, final int statusCode, final APIResponse response) {
+		this.sendResponse(exchange, statusCode, new Gson().toJson(response));
+	}
+
+	private void sendResponse(final HttpExchange exchange, final int statusCode, final String response) {
+		try {
+			exchange.sendResponseHeaders(statusCode, response.length());
+			exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+			exchange.getResponseBody().close();
+			LOGGER.debug(
+					"Sent " + statusCode + " response to " + exchange.getRemoteAddress().getAddress().getHostName());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private class APIResponse {
+		@SerializedName("status")
+		private String status;
+		
+		@SerializedName("message")
+		private String message;
+		
+		private APIResponse(final String status, final String message) {
+			this.status = status;
+			this.message = message;
+		}
 	}
 }
